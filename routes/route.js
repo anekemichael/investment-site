796220827fa0bc984681023c2c347805c4416af5
Starrex_Dev <collinsihezie6@gmail.com>
2021-12-28 19:33:38 +0100

    finishing templating

--- routes/route.js
+++ routes/route.js
@@ -52,4 +52,48 @@ router.get('/terms-of-use', function (req, res){
     res.render('terms-of-use')
 })
 
+router.get('/dash-board', function(req, res){
+    res.render('dash-board')
+})
+
+router.get('/profile', function(req, res){
+    res.render('profile')
+})
+
+router.get('/edit-profile', function(req, res){
+    res.render('edit-profile')
+})
+
+router.get('/invest', function(req, res){
+    res.render('invest')
+})
+
+
+router.get('/withdrawal', function(req, res){
+    res.render('withdrawal')
+})
+
+
+router.get('/reinvest', function(req, res){
+    res.render('reinvest')
+})
+
+router.get('/view-pending-history', function(req, res){
+    res.render('view-pending-history')
+})
+
+router.get('/view-confirmed-history', function(req, res){
+    res.render('view-confirmed-history')
+})
+
+router.get('/referral', function(req, res){
+    res.render('referral')
+})
+
+router.get('/referral-withdrawal', function(req, res){
+    res.render('referral-withdrawal')
+})
+
+
+
 module.exports = router
